7786a12cb0e8bc2a87d27a7eb67d3a1c8d52dc48
Mustafa KOCAGURBUZ <MustafaKOCAGURBUZ@mem.gov.tr>
2021-03-26 11:43:52 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1, urun2};
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi + " - " + urun.Fiyati + " - " + urun.Aciklama);
+                Console.WriteLine("-------------------------");
+            }
+
+            Console.WriteLine("----------- Metotlar --------------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+            Console.WriteLine("-------------------------");
+            sepetManager.Ekle2("Armut", "Yeşil Armut", 12, 10);
+            sepetManager.Ekle2("Elma", "Yeşil Elma", 10, 9);
+            sepetManager.Ekle2("Karpuz", "Adana Karpuzu", 60, 8);
+        }
+    }
+}
